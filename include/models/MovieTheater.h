7265d555a1ddc38b05f36d62ac9fa728e13ce6dd
VinnySimpSSZY <khacvuong2707@gmail.com>
2025-04-28 13:28:24 +0000

    Upload MovieTheater.h

---
+++ include/models/MovieTheater.h
@@ -0,0 +1,24 @@
+#ifndef MOVIE_THEATER_H
+#define MOVIE_THEATER_H
+#include "Movie.h"
+
+class MovieTheater {
+    private:
+    int _id_theater;
+    string _name;
+    string _location;
+    public:
+    MovieTheater();
+    ~MovieTheater();
+    MovieTheater(int id_theater, string name, string location);
+    //getter
+    int getTheaterId() const;
+    string getTheaterName() const;
+    string getTheaterLocation() const;
+    //setter
+    void setTheaterId(int id_theater);
+    void setTheaterName(string name);
+    void setTheaterLocation(string location);
+};
+
+#endif
