4c31ce245e29c90ccacccae0560497a04a54df7a
VinnySimpSSZY <khacvuong2707@gmail.com>
2025-04-28 15:34:55 +0000

    Edit Result.h

--- include/models/Result.h
+++ include/models/Result.h
@@ -8,7 +8,14 @@ template<typename T>
 struct Result {
     bool success;
     T data;
-    std::string errorMessage;
+    std::string error_message;
+
+    Result() {}
+    Result(bool success, T data, const std::string& error_message = "")
+        : success(success), data(data), errorMessage(error_message) {}
+    Result(bool success, const std::string& error_message = "")
+        : success(success), errorMessage(error_message) {}
+
 };
 
 
