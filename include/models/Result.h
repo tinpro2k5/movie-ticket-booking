c2e8a4c613b40a55d03bb81f451855adbd3c8bc2
VinnySimpSSZY <khacvuong2707@gmail.com>
2025-04-28 13:37:52 +0000

    Struct Result to check true or false case

---
+++ include/models/Result.h
@@ -0,0 +1,15 @@
+#ifndef _RESULT_H
+#define _RESULT_H
+
+#include <optional>
+#include <string>
+
+template<typename T>
+struct Result {
+    bool success;
+    T data;
+    std::string errorMessage;
+};
+
+
+#endi
