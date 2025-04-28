ca1ec968efd5f7db07664d4b4dc8b3e8325152e8
VinnySimpSSZY <khacvuong2707@gmail.com>
2025-04-28 17:21:58 +0000

    write function for MovieRepository

---
+++ include/repositories/MovieRepository.h
@@ -0,0 +1,20 @@
+#ifndef MOVIEREPOSITORY_H
+#define MOVIEREPOSITORY_H
+
+#include "include/models/Movie.h"
+#include "include/utils/DatabaseManager.h"
+#include "include/repositories/Result.h"
+class MovieRepository {
+    public:
+        //Get movie lists
+        Result<vector<Movie>> findAll();
+        //Get movie by id
+        Result<Movie> findById(int id_movie);
+        //Add movie
+        Result<bool> add(const Movie& movie);
+        //Update movie
+        Result<bool> update(const Movie& movie);
+        //Delete movie
+        Result<bool> remove(int id_movie);
+};
+#endif
