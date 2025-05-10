4f543d3f4bb11b63c4e052406a8a2efde984cb90
le trung tin <23120371@student.hcmus.edu.vn>
2025-05-10 14:52:03 +0000

    Remove unused dbManager and logger initialization from IRepository constructor

--- src/repositories/IRepository.cpp
+++ src/repositories/IRepository.cpp
@@ -2,13 +2,7 @@
 
 template <typename T>
 IRepository<T>::IRepository() {
-    dbManager = DatabaseManager::getInstance();
-    logger = Logger::getInstance();
-}
-template <typename T>
-IRepository<T>::~IRepository() {
-    // Destructor
-    // Clean up resources if needed
+    
 }
 
 template <typename T>
